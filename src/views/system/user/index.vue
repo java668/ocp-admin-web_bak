be63041933c13c9964e6eecfb9957c5c155e14be
陈江龙 <java668_real@163.com>
2023-07-27 23:42:11 +0800

    feat: 用户管理开发

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,158 +1,148 @@
 <template>
   <div class="container">
     <Breadcrumb :items="['menu.system', 'menu.system.user']" />
-    <a-card class="general-card">
-      <a-row>
-        <a-col :flex="1">
-          <a-form
-            :model="formModel"
-            :label-col-props="{ span: 6 }"
-            :wrapper-col-props="{ span: 18 }"
-            label-align="left"
-          >
-            <a-row :gutter="16">
-              <a-col :span="8">
-                <a-form-item
-                  field="number"
-                  :label="$t('searchTable.form.number')"
-                >
-                  <a-input
-                    v-model="formModel.number"
-                    :placeholder="$t('searchTable.form.number.placeholder')"
-                  />
-                </a-form-item>
-              </a-col>
-              <a-col :span="8">
-                <a-form-item field="name" :label="$t('searchTable.form.name')">
-                  <a-input
-                    v-model="formModel.name"
-                    :placeholder="$t('searchTable.form.name.placeholder')"
-                  />
-                </a-form-item>
-              </a-col>
-              <a-col :span="8">
-                <a-form-item
-                  field="contentType"
-                  :label="$t('searchTable.form.contentType')"
-                >
-                  <a-select
-                    v-model="formModel.contentType"
-                    :options="contentTypeOptions"
-                    :placeholder="$t('searchTable.form.selectDefault')"
-                  />
-                </a-form-item>
-              </a-col>
-              <a-col :span="8">
-                <a-form-item
-                  field="filterType"
-                  :label="$t('searchTable.form.filterType')"
-                >
-                  <a-select
-                    v-model="formModel.filterType"
-                    :options="filterTypeOptions"
-                    :placeholder="$t('searchTable.form.selectDefault')"
-                  />
-                </a-form-item>
-              </a-col>
-              <a-col :span="8">
-                <a-form-item
-                  field="createdTime"
-                  :label="$t('searchTable.form.createdTime')"
-                >
-                  <a-range-picker
-                    v-model="formModel.createdTime"
-                    style="width: 100%"
-                  />
-                </a-form-item>
-              </a-col>
-              <a-col :span="8">
-                <a-form-item
-                  field="status"
-                  :label="$t('searchTable.form.status')"
-                >
-                  <a-select
-                    v-model="formModel.status"
-                    :options="statusOptions"
-                    :placeholder="$t('searchTable.form.selectDefault')"
-                  />
-                </a-form-item>
-              </a-col>
-            </a-row>
-          </a-form>
-        </a-col>
-        <a-divider style="height: 84px" direction="vertical" />
-        <a-col :flex="'86px'" style="text-align: right">
-          <a-space direction="vertical" :size="18">
-            <a-button type="primary" @click="search">
-              <template #icon>
-                <icon-search />
-              </template>
-              {{ $t('searchTable.form.search') }}
-            </a-button>
-            <a-button @click="reset">
-              <template #icon>
-                <icon-refresh />
-              </template>
-              {{ $t('searchTable.form.reset') }}
-            </a-button>
-          </a-space>
-        </a-col>
-      </a-row>
+    <a-card>
+      <a-form label-align="right" auto-label-width :model="form" class="form">
+        <a-row :gutter="16" wrap>
+          <a-col :xs="12" :md="12" :lg="8" :xl="6" :xxl="6">
+            <a-form-item field="value1" label="姓名">
+              <a-input v-model="form.value1" placeholder="请输入姓名" />
+            </a-form-item>
+          </a-col>
+          <a-col :xs="12" :md="12" :lg="8" :xl="6" :xxl="6">
+            <a-form-item field="value2" label="手机">
+              <a-input v-model="form.value2" placeholder="请输入手机号码" />
+            </a-form-item>
+          </a-col>
+          <a-col v-show="collapsed" :xs="12" :md="12" :lg="8" :xl="6" :xxl="6">
+            <a-form-item field="value3" label="类型">
+              <a-select placeholder="请选择">
+                <a-option>北京</a-option>
+                <a-option>上海</a-option>
+                <a-option>广州</a-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col v-show="collapsed" :xs="12" :md="12" :lg="8" :xl="6" :xxl="6">
+            <a-form-item field="value3" label="时间">
+              <a-date-picker
+                v-model="form.value3"
+                show-time
+                placeholder="请选择创建时间"
+                style="width: 100%"
+              />
+            </a-form-item>
+          </a-col>
+          <a-col v-show="collapsed" :xs="12" :md="12" :lg="8" :xl="6" :xxl="6">
+            <a-form-item field="value4" label="状态">
+              <a-select placeholder="请选择">
+                <a-option>开启</a-option>
+                <a-option>关闭</a-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col v-show="collapsed" :xs="12" :md="12" :lg="8" :xl="6" :xxl="6">
+            <a-form-item field="value5" label="地址">
+              <a-input v-model="form.value5" placeholder="请输入查询地址" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-space>
+              <a-button type="primary">
+                <template #icon>
+                  <icon-search />
+                </template>
+                <template #default>查询</template>
+              </a-button>
+              <a-button>
+                <template #default>重置</template>
+              </a-button>
+              <a-button
+                type="text"
+                class="collapsed-btn"
+                @click="collapsed = !collapsed"
+              >
+                <template #icon>
+                  <icon-up v-if="collapsed" />
+                  <icon-down v-else />
+                </template>
+                <template #default>{{ !collapsed ? '展开' : '收起' }}</template>
+              </a-button>
+            </a-space>
+          </a-col>
+        </a-row>
+      </a-form>
+      <a-divider style="margin-top: 0" />
+      <a-table :columns="columns" :data="data" />
     </a-card>
   </div>
 </template>
 
-<script setup>
-  import {computed, ref} from "vue";
-  import {SelectOptionData} from "@arco-design/web-vue/es/select/interface";
-  import {useI18n} from "vue-i18n/dist/vue-i18n";
+<script lang="ts" setup>
+  import { ref, reactive } from 'vue';
 
-  const generateFormModel = () => {
-    return {
-      number: '',
-      name: '',
-      contentType: '',
-      filterType: '',
-      createdTime: [],
-      status: '',
-    };
-  };
-  const {t} = useI18n();
+  const form = reactive({
+    value1: '',
+    value2: '',
+    value3: '',
+    value4: '',
+    value5: '',
+  });
+  const collapsed = ref(false);
 
-  const formModel = ref(generateFormModel());
-
-  const contentTypeOptions = computed <SelectOptionData[]> (() => [
+  const columns = [
     {
-      label: t('searchTable.form.contentType.img'),
-      value: 'img',
+      title: 'Name',
+      dataIndex: 'name',
     },
     {
-      label: t('searchTable.form.contentType.horizontalVideo'),
-      value: 'horizontalVideo',
+      title: 'Salary',
+      dataIndex: 'salary',
     },
     {
-      label: t('searchTable.form.contentType.verticalVideo'),
-      value: 'verticalVideo',
-    },
-  ]);
-  const filterTypeOptions = computed < SelectOptionData[] > (() => [
-    {
-      label: t('searchTable.form.filterType.artificial'),
-      value: 'artificial',
+      title: 'Address',
+      dataIndex: 'address',
     },
     {
-      label: t('searchTable.form.filterType.rules'),
-      value: 'rules',
+      title: 'Email',
+      dataIndex: 'email',
     },
-  ]);
-  const statusOptions = computed < SelectOptionData[] > (() => [
+  ];
+  const data = reactive([
     {
-      label: t('searchTable.form.status.online'),
-      value: 'online',
+      key: '1',
+      name: 'Jane Doe',
+      salary: 23000,
+      address: '32 Park Road, London',
+      email: 'jane.doe@example.com',
     },
     {
-      label: t('searchTable.form.status.offline'),
-      value: 'offline',
+      key: '2',
+      name: 'Alisa Ross',
+      salary: 25000,
+      address: '35 Park Road, London',
+      email: 'alisa.ross@example.com',
+    },
+    {
+      key: '3',
+      name: 'Kevin Sandra',
+      salary: 22000,
+      address: '31 Park Road, London',
+      email: 'kevin.sandra@example.com',
+    },
+    {
+      key: '4',
+      name: 'Ed Hellen',
+      salary: 17000,
+      address: '42 Park Road, London',
+      email: 'ed.hellen@example.com',
+    },
+    {
+      key: '5',
+      name: 'William Smith',
+      salary: 27000,
+      address: '62 Park Road, London',
+      email: 'william.smith@example.com',
     },
   ]);
 </script>
