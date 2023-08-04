8c5ea5cf922ac186d8af4da40b4357f090323d77
java668 <12401017+java668_real@user.noreply.gitee.com>
2023-08-04 19:41:33 +0800

    feat: 角色管理开发

--- src/api/user.ts
+++ src/api/user.ts
@@ -5,13 +5,20 @@ import { UserState } from '@/store/modules/user/types';
 export interface LoginData {
   username: string;
   password: string;
+  validCode: string;
+  deviceId: string;
+  grant_type: string;
 }
 
 export interface LoginRes {
   token: string;
 }
 export function login(data: LoginData) {
-  return axios.post<LoginRes>('/api/user/login', data);
+  const CLIENT_ID = import.meta.env.CLIENT_ID;
+  const CLIENT_SECRET = import.meta.env.CLIENT_SECRET;
+  console.log("CLIENT_ID", CLIENT_ID)
+  console.log("CLIENT_SECRET", CLIENT_SECRET)
+  return axios.post<LoginRes>('/api-auth/oauth/token', data);
 }
 
 export function logout() {
